c6571a22ab4ccb83ce4d4cb205286da426ef36b7
LucasAntunesDev <lucasantuneslara06@gmail.com>
2024-03-15 09:15:56 -0300

    requisições nos métodos get e post

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express')
 
+const PORT = 3000
 //criando o servidor
 const app = express()
 
+// underline => usado pra ignorar um parâmetro
+app.get('/teste', (_, res) => {
+    res.send('Você enviou uma requisição GET!')
+})
+
+app.post('/', (_, res) => {
+    res.send('Você enviou uma requisição POST!')
+})
+
 //definindo a porta em que o servidor vai funcionar
-app.listen(3000, () => {console.log('Servidor rodando na porta 3000')})
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}!`))

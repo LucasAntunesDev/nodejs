94a890a2fc2f35daf97f46bc9b5dd67047742ee8
LucasAntunesDev <lucasantuneslara06@gmail.com>
2024-03-15 08:44:57 -0300

    criação do servidor

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('oi')
+const express = require('express')
+
+//criando o servidor
+const app = express()
+
+//definindo a porta em que o servidor vai funcionar
+app.listen(3000, () => {console.log('Servidor rodando na porta 3000')})

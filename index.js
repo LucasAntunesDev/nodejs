85f0c0620d35ad24adc2e2da446f61631a0c0078
LucasAntunesDev <lucasantuneslara06@gmail.com>
2024-03-15 10:02:11 -0300

    outros métodos e uso de requisições

--- index.js
+++ index.js
@@ -5,12 +5,25 @@ const PORT = 3000
 const app = express()
 
 // underline => usado pra ignorar um parâmetro
-app.get('/teste', (_, res) => {
+app.get('/', (_, res) => {
     res.send('Você enviou uma requisição GET!')
+})
+
+app.get('/cidade/:cidade', (req, res) => {
+    const {cidade} = req.params
+    res.send(`Você está na cidade ${cidade}!`)
 })
 
 app.post('/', (_, res) => {
     res.send('Você enviou uma requisição POST!')
+})
+
+app.delete('/', (_, res) => {
+    res.send('Você enviou uma requisição DELETE!')
+})
+
+app.put('/', (_, res) => {
+    res.send('Você enviou uma requisição PUT!')
 })
 
 //definindo a porta em que o servidor vai funcionar

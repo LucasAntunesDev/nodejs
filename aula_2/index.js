d70f322ed982147d2b5c0ff1687ca5cfcbf06ca0
LucasAntunesDev <lucasantuneslara06@gmail.com>
2024-04-01 09:16:30 -0300

    consulta ao banco de dados e retorno deles em um json - aula 2

--- aula_2/index.js
+++ aula_2/index.js
@@ -5,14 +5,33 @@ const mysql = require('mysql2/promise')
 const PORT = 3000
 const app = express()
 
-app.get('/', (req, res) => {
-    const conn = mysql.createConnection({
+/*Define a função como assíncrona, para que a constante *conn* funcione com o await */
+app.get('/', async (req, res) => {
+    /*Promessa => evento assíncrono, espera algo acontecer
+    * [antigamente] chama um callback com duas possibilidades : sucesso ou erro
+    * [hoje] usar async e await
+    */
+    const conn = await mysql.createConnection({
         host: 'localhost',
         user: 'root',
         database: 'aula_01_04'
     })
-    console.log(conn)
-    res.send('<h1>Hello World!</h1>')
+    const [resultado] = await conn.query({
+        sql : 'SELECT id, nome FROM pessoas'
+    })
+    res.json(resultado)
+    /**
+     **Desestruturação**: 
+     * no PHP:
+     * const vetor = [1,2,3]
+     * const a = vetor[0]
+     * const b = vetor[1]
+     * const c = vetor[2]
+     * 
+     * No JS:
+     * const [a,b,c] = vetor
+     * cosnt [a,b,c] = [1,2,3]
+     */
 })
 
 app.listen(PORT, () => {
